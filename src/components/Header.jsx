a6fa32d7c315c7e195c025508cd7ae08304dc456
otan-321 <germanogerale@gmail.com>
2026-06-05 10:56:27 +0800

    Replace header icon with custom logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
-import { BookOpen, Trophy } from 'lucide-react';
-
+import { Trophy } from 'lucide-react';
+import { BookOpen } from 'lucide-react';
 function Header() {
   return (
     <header className="bg-white dark:bg-gray-950 shadow-md sticky top-0 z-50">
       <div className="container mx-auto px-4 py-3">
         <div className="flex items-center justify-between">
           <Link to="/" className="flex items-center space-x-2 group">
-            <BookOpen className="w-8 h-8 text-blue-600 group-hover:text-blue-700 transition-colors" />
+            <img src="/logo.png" alt="Logo" className="w-8 h-8" />
             <div>
               <h1 className="text-sm md:text-sm font-bold bg-linear-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
                 CSE Reviewer
@@ -15,19 +15,17 @@ function Header() {
               <p className="text-xs text-gray-500 dark:text-gray-100">Your Go-To Reviewer</p>
             </div>
           </Link>
-
           <nav className="flex items-center space-x-4">
-            <Link 
-              to="/" 
+            <Link
+              to="/"
               className="flex items-center space-x-1 py-2 rounded-lg hover:bg-blue-100 hover:dark:bg-gray-800 dark:bg-gray-950 transition-colors text-gray-700 dark:text-gray-200 hover:text-blue-600 md:text-sm"
             >
               <BookOpen className="w-4 h-4" />
               <span>Home</span>
             </Link>
-            
-            <Link 
-              to="/results" 
-              className="flex items-center space-x-1 py-2 rounded-lg hhover:bg-blue-100 hover:dark:bg-gray-800 dark:bg-gray-950 transition-colors text-gray-700 dark:text-gray-200 hover:text-blue-600 md:text-sm"
+            <Link
+              to="/results"
+              className="flex items-center space-x-1 py-2 rounded-lg hover:bg-blue-100 hover:dark:bg-gray-800 dark:bg-gray-950 transition-colors text-gray-700 dark:text-gray-200 hover:text-blue-600 md:text-sm"
             >
               <Trophy className="w-4 h-4" />
               <span>Results</span>
@@ -38,5 +36,4 @@ function Header() {
     </header>
   );
 }
-
 export default Header;
